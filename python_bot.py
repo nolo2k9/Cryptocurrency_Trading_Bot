a881e3299e66c3ba400a18e84d95ecce7c3bc7a7
Keith Nolan <g00351932@gmit.ie>
2021-02-14 19:38:54 +0000

    Set up inital project and connected socket

---
+++ python_bot.py
@@ -0,0 +1,14 @@
+import websocket
+#Socket and address
+SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+def on_open(ws):
+    print('opened connection')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message):
+    print('recieved message')
+    #global closes, in_position
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever()
