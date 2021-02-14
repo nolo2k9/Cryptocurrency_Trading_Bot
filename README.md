b82a9d59948eddfae3674d9c24faeaa7966ec9fd
Keith Nolan <g00351932@gmit.ie>
2021-02-14 20:07:16 +0000

    Printing data out from api. Collecting closing prices from each candle stick

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Cryptocurrency_Trading_Bot
+#Pre-reqs:
+Pip
+
+
+# How to run project
+Set up virtual environment:
+https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+
+Install requirements.txt:
+pip3 -r requirements.txt  
+
+***References***
+
+# Virtual environment
+https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+
+# TA_LIB
+https://pypi.org/project/TA-Lib/
+
+# Binance Api
+https://github.com/sammchardy/python-binance
--- python_bot.py
+++ python_bot.py
@@ -1,14 +1,36 @@
-import websocket
+import websocket, json, pprint
 #Socket and address
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+
+closes = []
+
+#Open socet connection
 def on_open(ws):
     print('opened connection')
-
+    
+#Close socket connection
 def on_close(ws):
     print('closed connection')
-
+    
+#Print message recieved in json format
 def on_message(ws, message):
-    print('recieved message')
-    #global closes, in_position
+    global closes
+    print(message)
+    #convert json string to python data format
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+   
+    candle = json_message['k']
+
+    is_candle_closed = candle['x']
+    close = candle['c']
+    #Print candle closing price
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
+        #Appending closing prices
+        closes.append(float(close))
+        print("closes")
+        print(closes)
+        
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
